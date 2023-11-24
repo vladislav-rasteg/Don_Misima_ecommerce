37f36438fd5cb67e4a1568e668f7c8ba5fb6994b
vladrast <rastegaev.design@gmail.com>
2023-11-24 20:02:03 +0500

    feat: fix hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -13,6 +13,8 @@ const Hero = () => {
 
     const ref = useRef<HTMLDivElement>(null)
 
+    const maxUp = window.innerHeight * 1.5 - window.innerHeight * 1.3;
+
     const options = {
         smooth: true,
         multiplier: 3,
@@ -20,60 +22,57 @@ const Hero = () => {
 
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["end start", "end end"]
+        offset: ["0 0", "2 1"]
     })
-
-    scrollYProgress.on("change", (() => {
-        console.log(scrollYProgress.getVelocity())
-    }))
-
+    
     const margin1 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["120px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp}px`])
 
     const margin2 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["105px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp}px`])
 
     const margin3 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["90px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp/1.5}px`])
 
     const margin4 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["75px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp/2}px`])
 
     const margin5 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["60px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp/2.5}px`])
 
     const margin6 = useTransform(useSpring(scrollYProgress, {
         stiffness: 200,
         damping: 30,
         restDelta: 0.001
-    }), [0, 1], ["45px", `0px`])
+    }), [0, 1], ["0px", `-${maxUp/3}px`])
 
     return(
-        <div className={s.firstBlock} ref={ref}>
+        <div className={s.firstBlock}>
             <div className={s.dark}>
                 <MainPageLogo />
             </div>
-            <motion.img className={s.heroImage} src={HeroImage1} style={{bottom: margin1}} alt="tobacco field"/>
-            <motion.img className={s.heroImage} src={HeroImage2} alt="tobacco field"/>
-            <motion.img className={s.heroImage} src={HeroImage3} alt="tobacco field"/>
-            <motion.img className={s.heroImage} src={HeroImage4} alt="tobacco field"/>
-            <motion.img className={s.heroImage} src={HeroImage5} alt="tobacco field"/>
-            <motion.img className={s.heroImage} src={HeroImage6} alt="tobacco field"/>
+            <div className={s.scrollContainer} ref={ref}></div>
+            <motion.img className={s.heroImage} src={HeroImage1} style={{top: margin1, zIndex: 7}} alt="tobacco field"/>
+            <motion.img className={s.heroImage} src={HeroImage2} style={{top: margin2, zIndex: 6}} alt="tobacco field"/>
+            <motion.img className={s.heroImage} src={HeroImage3} style={{top: margin3, zIndex: 5}} alt="tobacco field"/>
+            <motion.img className={s.heroImage} src={HeroImage4} style={{top: margin4, zIndex: 4}} alt="tobacco field"/>
+            <motion.img className={s.heroImage} src={HeroImage5} style={{top: margin5, zIndex: 3}} alt="tobacco field"/>
+            <motion.img className={s.heroImage} src={HeroImage6} style={{top: margin6, zIndex: 2}} alt="tobacco field"/>
         </div>
     )
 }
